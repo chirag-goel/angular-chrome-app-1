4739c4cb32da11319f57b14b6f2a948f3b7efe46
Rob C <rm.rf.etc@gmail.com>
2014-02-19 12:20:10 -0800

    Added minification for css & js.

--- gruntfile.js
+++ gruntfile.js
@@ -1,29 +1,45 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    sass: {
-      dist: {
-        files: {
-          'assets/styles/style.css': 'src/sass/*.scss',
-        }
-      }
-    },
     browserify: {
       dist: {
-        files: {
-          'assets/scripts/app.js': 'src/js/*.js'
-        }
+        files: [
+          { src: 'src/js/*.js', dest: 'assets/scripts/app.js' }
+        ]
+      }
+    },
+    ngmin : {
+      dist: {
+        files: [
+          { src: 'assets/scripts/app.js', dest: 'assets/scripts/app.min.js' }
+        ]
+      }
+    },
+    sass: {
+      dist: {
+        files: [
+          { src: 'assets/styles/main.css', dest: 'src/sass/*.scss' }
+        ]
+      }
+    },
+    cssmin: {
+      css: {
+        files: [
+          { src: 'assets/styles/main.css', dest: 'assets/styles/main.min.css' }
+        ]
       }
     },
     watch: {
       sass: {
         files: ['src/sass/*.scss', 'src/js/**/*'],
-        tasks: ['sass', 'browserify']
+        tasks: ['sass', 'browserify', 'cssmin', 'ngmin']
       }
     }
   })
   grunt.loadNpmTasks('grunt-sass')
+  grunt.loadNpmTasks('grunt-ngmin')
   grunt.loadNpmTasks('grunt-browserify')
   grunt.loadNpmTasks('grunt-contrib-watch')
-  grunt.registerTask('default', ['sass:dist', 'watch', 'browserify:dist'])
+  grunt.loadNpmTasks('grunt-contrib-cssmin')
+  grunt.registerTask('default', ['sass:dist', 'cssmin', 'watch', 'browserify:dist', 'ngmin:dist'])
 }
